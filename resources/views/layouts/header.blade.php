cc832567c5243cf1f9a394fb71fe5f222c8a4b2d
Domenico Capalbo <capalbodomenico@gmail.com>
2020-11-05 20:56:43 +0100

    front-end-navbar-films-card-hover-effect
    website-fabrique-entertainment

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -12,7 +12,14 @@
    <nav class="header_nav">
     <li><a href='{{route('home')}}'>Home</a></li>
     <li><a href='{{route('about')}}'>About</a></li>
-    <li><a href='{{route('projects')}}'>Projects</a></li>
+    <li class="dropdown_list"><a href='{{route('projects')}}'>Projects</a>
+    {{-- dropdown projects menu --}}
+        {{-- <li class="list_item">Lucania</li>
+        <li class="list_item">Love and Desire</li>
+        <li class="list_item">Nightlife</li>
+        <li class="list_item">Guerrieri</li> --}}
+    </li>
+    {{-- end dropdown menu --}}
     <li><a href='{{route('news')}}'>News</a></li>
     <li><a href='{{route('contacts')}}'>Contacts</a></li>
    </nav>
