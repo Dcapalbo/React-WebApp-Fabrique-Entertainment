35d1ca5bd8eead7f9fa786f12f715a1c2d7c286d
Domenico Capalbo <capalbodomenico@gmail.com>
2021-01-03 08:13:03 +0100

    wip-footer-column-added

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,8 +1,29 @@
 <section class="footer_section">
     <div class="footer_wrapper">
-        <div class="footer_column_item"></div>
-        <div class="footer_column_item"></div>
-        <div class="footer_column_item"></div>
-        <div class="footer_column_item"></div>
+        <div class="footer_column_item">
+            <ul class="column_item">
+                <li class="list_item">Home</li>
+                <li class="list_item">About</li>
+                <li class="list_item">Projects</li>
+                <li class="list_item">News</li>
+                <li class="list_item">Contacts</li>
+            </ul>
+        </div>
+        <div class="footer_column_item">
+            <ul class="column_item">
+                <li class="list_item">Stay in contact</li>
+                <li class="list_item">Working with us</li>
+                <li class="list_item">Work in progress</li>
+                <li class="list_item">pre-production</li>
+            </ul>
+        </div>
+        <div class="footer_column_item">
+            <ul class="column_item">
+                <li class="list_item">Documents</li>
+                <li class="list_item">Blog</li>
+                <li class="list_item"></li>
+                <li class="list_item"></li>
+            </ul>
+        </div>
     </div>
 </section>
