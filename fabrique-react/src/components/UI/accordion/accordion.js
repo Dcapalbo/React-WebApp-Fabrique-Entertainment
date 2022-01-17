40ec189b2977a6de34f07b0f186803426552097b
Domenico Capalbo <capalbodomenico@gmail.com>
2022-01-17 16:29:33 +0100

    fixing-accordion-structure

--- fabrique-react/src/components/UI/accordion/accordion.js
+++ fabrique-react/src/components/UI/accordion/accordion.js
@@ -7,6 +7,12 @@ const Accordion = () => {
 const [selected, setSelected] = useState(null);
 
 const toggleAccordion = (i) => {
+    let clickedElm = document.querySelector(`.${classes.accordion__description__show}`);
+    console.log(clickedElm);
+    if (clickedElm) {
+        clickedElm.classList.remove(`${classes.accordion__description__show}`);
+        clickedElm.classList.add(`${classes.accordion__description}`);
+    }
     if (selected === i) {
         return setSelected(null);
     }
@@ -16,13 +22,17 @@ const toggleAccordion = (i) => {
         <div className={classes.wrapper__accordion}>
             <div className={classes.accordion}>
                 {accordionData.map((item, i) => (
-                   <div className={classes.item} onClick={() => toggleAccordion(i)}>
-                       <div className={classes.accordion__title}>
-                           <h2 className={selected === i ? `${classes.active}` : ''}>{item.headline}</h2>
+                   <div key={i} className={classes.item} onClick={() => toggleAccordion(i, item)}>
+                       <div>
+                           <h2 className={selected === i ? `${classes.active}` : `${classes.accordion__title}`}>{item.headline}</h2>
                        </div>
                         <div className={
-                            selected === i ? `${classes.accordion__description__show}` : `${classes.accordion__description}`
-                            }>{item.description}
+                            i === 0 && i !== 2 && i !== 1 ? 
+                            `${classes.accordion__description__show}` : selected === i ? 
+                            `${classes.accordion__description__show}` : `${classes.accordion__description}`
+                            }
+                        >
+                            {item.description}
                         </div>
                    </div> 
                 ))}
@@ -44,6 +54,7 @@ const accordionData = [
         headline: 'Team',
         description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
     }
+
 ]
 
 export default Accordion;
