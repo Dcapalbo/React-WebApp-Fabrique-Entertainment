62935273a4bf76dc89940d18f82d1d276bf9d180
Domenico Capalbo <capalbodomenico@gmail.com>
2022-01-21 23:10:37 +0100

    adding-footer-structure

--- fabrique-react/src/components/footer/footer.js
+++ fabrique-react/src/components/footer/footer.js
@@ -3,9 +3,14 @@ import classes from './footer.module.scss';
 const Footer = () => {
     return(
         <footer className={classes.footer}>
-            <div></div>
-            <div></div>
-            <div></div>
+            <div className={classes.footer__container}>
+                <div className={classes.footer__container__elm}>
+
+                </div>
+                <div className={classes.footer__container__elm}>
+                    
+                </div>
+            </div>
         </footer>
     )
 }
