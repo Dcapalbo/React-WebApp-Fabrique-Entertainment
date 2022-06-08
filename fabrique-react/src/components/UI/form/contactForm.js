36aef82cc4e9728491ef54d433fd148fd7b04570
Domenico Capalbo <capalbodomenico@gmail.com>
2022-06-08 18:39:19 +0200

    fix

--- fabrique-react/src/components/UI/form/contactForm.js
+++ fabrique-react/src/components/UI/form/contactForm.js
@@ -8,25 +8,25 @@ const contactForm = (props) => {
             <form className={classes.form__container}>
                 <h4>Vuoi scoprirne di più? Mettiamoci in contatto</h4>
                 <div className={classes.form__container__item}>
-                    <label for="Nome">
+                    <label htmlFor="Nome">
                         Nome
                     </label>
                     <input type="text" id="formName" value="Nome" maxlenght="100" minlenght="5" required></input>
                 </div>
                 <div className={classes.form__container__item}>
-                    <label for="Cognome">
+                    <label htmlFor="Cognome">
                         Cognome
                     </label>
                     <input type="text" id="formSurname" value="Cognome" maxlenght="100" minlenght="5" required></input>
                 </div>
                 <div className={classes.form__container__item}>
-                    <label for="Numero di telefono">
+                    <label htmlFor="Numero di telefono">
                         Numero di Telefono
                     </label>
                     <input type="text" id="formPhoneNumber" value="Numero di telefono" maxlenght="10" minlenght="10" required></input>
                 </div>
                 <div className={classes.form__container__item}>
-                    <label for="Email">
+                    <label htmlFor="Email">
                         Email
                     </label>
                     <input type="email" id="formEmail" value="Email" maxlenght="100" minlenght="20" required></input>
