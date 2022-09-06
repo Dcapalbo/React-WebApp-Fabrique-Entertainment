9036bf227f94fa93c38900d079457fb4621fba90
Domenico Capalbo <capalbodomenico@gmail.com>
2022-09-06 16:56:16 +0200

    wip menu mobile

--- fabrique-react/src/components/header/header.js
+++ fabrique-react/src/components/header/header.js
@@ -1,11 +1,14 @@
 import Navigation from '../nav/nav';
+import NavigationMobile from '../nav/navMobile';
 import classes from './header.module.scss';
 
 const Header = () => {
+    let domWidth = window.innerWidth;
     return(
         <header className={classes.header}>
             <div className={classes.header__container}>
-                <Navigation />
+                {domWidth >= 767 && <Navigation />}
+                {domWidth <= 767 && <NavigationMobile/>}   
             </div>
         </header>
     )
--- fabrique-react/src/components/nav/nav.js
+++ fabrique-react/src/components/nav/nav.js
@@ -1,18 +1,7 @@
 import classes from './nav.module.scss';
-import { useState } from 'react';
 import logo from '../../assets/img/LOGO_Fabrique_Entertainment_White_PNG.png';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { solid } from '@fortawesome/fontawesome-svg-core/import.macro';
 
 const Navigation = () => {
-
-    const [visible, setIsVisible] = useState(false);
-    const handlerMobilemenu = () => {
-        setIsVisible(true);
-    }
-
-    let domWidth = window.innerWidth;
-    if (domWidth >= 767) {
         return(
             <nav className={classes.navigation}>
                 <a href="/">
@@ -43,45 +32,6 @@ const Navigation = () => {
                 </ul>
             </nav>
         )
-    } else {
-        return (
-            <>
-                <nav className={classes.navigation}>
-                    <a href="/">
-                        <img className={classes.navigation__logo} src={logo} alt="logo fabrique entertainment" />
-                    </a>
-                    <div>
-                        { visible === false && <FontAwesomeIcon onClick={ handlerMobilemenu } className={classes.bars} icon={solid('bars')}/> }
-                        { visible && 
-                            <ul className={classes.navigation + ' ' + classes.mobile__navigation}>
-                                <li>
-                                    <a href="/">home</a>
-                                </li>
-                                <li>
-                                    <a href="/about">about</a>
-                                </li>
-                                <li>
-                                    <a className={classes.navigation__films} href="/films">films</a>
-                                    <ul className={classes.navigation__films__dropdown}>
-                                        <li><a href="/films/lucania">lucania</a></li>
-                                        <li><a href="/films/guerrieri">guerrieri</a></li>
-                                        <li><a href="/films/love-and-desire">love and desire</a></li>
-                                        <li><a href="/films/felakuti">felakuti</a></li>
-                                    </ul>
-                                </li>
-                                <li>
-                                    <a href="/news">news</a>
-                                </li>
-                                <li>
-                                    <a href="/contact">contact</a>
-                                </li>
-                             </ul> 
-                        } 
-                    </div>
-                </nav>
-            </>
-        )
     }
-}
 
 export default Navigation;
