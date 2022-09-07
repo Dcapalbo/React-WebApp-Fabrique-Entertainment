f5baaf8fe5b4545c8c01767520b620f1b1029815
Domenico Capalbo <capalbodomenico@gmail.com>
2022-09-07 17:07:21 +0200

    work in progress mobile responsive, state trigger navmodal problem

--- fabrique-react/src/components/header/header.js
+++ fabrique-react/src/components/header/header.js
@@ -7,23 +7,28 @@ import Navigation from '../nav/nav';
 import { useState } from 'react';
 
 const Header = () => {
+
     let domWidth = window.innerWidth;
     const [visible, setIsVisible] = useState(false);
     const handlerMobilemenu = () => {
-        console.log(visible)
+        const body = document.querySelector("body");
+        body.style.overflow = 'hidden';
         setIsVisible(true);
     }
+
     return(
         <header className={classes.header}>
             <div className={classes.header__container}>
                 {domWidth >= 767 && <Navigation />}
-                {domWidth <= 767 && <NavigationMobile/>}
+                {domWidth <= 767 && !visible && <NavigationMobile/>}
                 {domWidth <= 767 && !visible && 
-                    <FontAwesomeIcon 
-                        onClick={ handlerMobilemenu } 
-                        style={{color: "white", font: "20px", cursor: "pointer"}} 
-                        icon={solid('bars')}
-                    />
+                    <div className={classes.header__bars__container}>
+                        <FontAwesomeIcon 
+                            onClick={ handlerMobilemenu } 
+                            style={{color: "white", font: "20px", cursor: "pointer"}} 
+                            icon={solid('bars')}
+                        />
+                    </div>
                 }
                 {visible && <NavModal />}     
             </div>
--- fabrique-react/src/components/modal/navModal.js
+++ fabrique-react/src/components/modal/navModal.js
@@ -1,33 +1,46 @@
 import classes from './navModal.module.scss';
 
-const navModal = () => {
+const NavModal = () => {
+
+    const closingModalHandler = () => {
+       let navigationModalMobile = document.querySelector(".navModal_navigation__modal__LjcJz");
+       let body = document.querySelector("body");
+       navigationModalMobile.style.display = "none";
+       body.style.overflow = 'scroll';
+    }
+
     return(
-        <div className={classes.navigation__modal}>
-            <ul className={classes.navigation__mobile__menu}>
-                <li>
-                    <a href="/">home</a>
-                </li>
-                <li>
-                    <a href="/about">about</a>
-                </li>
-                <li>
-                    <a className={classes.navigation__films__mobile} href="/films">films</a>
-                    <ul className={classes.navigation__films__mobile__dropdown}>
-                        <li><a href="/films/lucania">lucania</a></li>
-                        <li><a href="/films/guerrieri">guerrieri</a></li>
-                        <li><a href="/films/love-and-desire">love and desire</a></li>
-                        <li><a href="/films/felakuti">felakuti</a></li>
-                    </ul>
-                </li>
-                <li>
-                    <a href="/news">news</a>
-                </li>
-                <li>
-                    <a href="/contact">contact</a>
-                </li>
-            </ul> 
-        </div>
-    );
+            <>
+                <div className={classes.navigation__modal}>
+                    <div className={classes.navigation__modal__close}>
+                        <span onClick={closingModalHandler}>X</span>
+                    </div>
+                    <ul className={classes.navigation__mobile__menu}>
+                        <li>
+                            <a href="/">home</a>
+                        </li>
+                        <li>
+                            <a href="/about">about</a>
+                        </li>
+                        <li>
+                            <a className={classes.navigation__films__mobile} href="/films">films</a>
+                            <ul className={classes.navigation__films__mobile__dropdown}>
+                                <li><a href="/films/lucania">lucania</a></li>
+                                <li><a href="/films/guerrieri">guerrieri</a></li>
+                                <li><a href="/films/love-and-desire">love and desire</a></li>
+                                <li><a href="/films/felakuti">felakuti</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="/news">news</a>
+                        </li>
+                        <li>
+                            <a href="/contact">contact</a>
+                        </li>
+                    </ul> 
+                </div>
+            </>
+    )
 }
 
-export default navModal;
+export default NavModal;
