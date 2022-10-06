739a9f2ad4688052838647edb1cac675b686e9f1
Domenico Capalbo <capalbodomenico@gmail.com>
2022-10-06 22:32:56 +0200

    working on the form controls

--- fabrique-react/src/components/UI/form/contactForm.js
+++ fabrique-react/src/components/UI/form/contactForm.js
@@ -1,7 +1,40 @@
 import classes from './contactForm.module.scss';
+import { useState } from 'react';
 
-// FORMIK adding the form TO DO 
-const contactForm = (props) => {
+
+const ContactForm = () => {
+
+    const [formData, setFormData] = useState([]);
+    const [surname, setSurname] = useState('');
+    const [number, setNumber] = useState('');
+    const [email, setEmail] = useState('');
+    const [name, setName] = useState('');
+
+    const onChangeHandlerSurname = (event) => {
+        setSurname(event.target.value);
+    }
+
+    const onChangeHandlerNumber = (event) => {
+        setNumber(event.target.value);
+        if (number === 2) {
+            console.log("dio cane");
+        }
+    }
+
+    const onChangeHandlerEmail = (event) => {
+        setEmail(event.target.value);
+    }
+
+    const onChangeHandlerName = (event) => {
+        setName(event.target.value);
+    }
+
+    const onClickHandler = () => {
+        setFormData([surname, number, email, name]);
+    } 
+
+    console.log(formData);
+
     return(
         <section className={classes.form__wrapper}>
             <form className={classes.form__container}>
@@ -10,29 +43,66 @@ const contactForm = (props) => {
                     <label htmlFor="Nome">
                         Nome
                     </label>
-                    <input type="text" id="formName" name="Nome" maxlenght="100" minlenght="5" required></input>
+                    <input
+                        onChange={onChangeHandlerName}
+                        type="text" 
+                        id="formName" 
+                        name="Nome" 
+                        required
+                        >    
+                    </input>
                 </div>
                 <div className={classes.form__container__item}>
                     <label htmlFor="Cognome">
                         Cognome
                     </label>
-                    <input type="text" id="formSurname" name="Cognome" maxlenght="100" minlenght="5" required></input>
+                    <input 
+                        onChange={onChangeHandlerSurname}
+                        type="text" 
+                        id="formSurname" 
+                        name="Cognome" 
+                        required
+                        >
+                    </input>
                 </div>
                 <div className={classes.form__container__item}>
                     <label htmlFor="Numero di telefono">
                         Numero di Telefono
                     </label>
-                    <input type="text" id="formPhoneNumber" name="Numero di telefono" maxlenght="10" minlenght="10" required></input>
+                    <input
+                        onChange={onChangeHandlerNumber}
+                        type="number" 
+                        id="formPhoneNumber" 
+                        name="Numero di telefono"
+                        required
+                        >
+                    </input>
                 </div>
                 <div className={classes.form__container__item}>
                     <label htmlFor="Email">
                         Email
                     </label>
-                    <input type="email" id="formEmail" name="Email" maxlenght="100" minlenght="20" required></input>
+                    <input 
+                        onChange={onChangeHandlerEmail}
+                        type="email" 
+                        id="formEmail" 
+                        name="Email" 
+                        required
+                        >
+                    </input>
+                </div>
+                <div className={classes.form__container__item}>
+                    <button
+                        onClick={onClickHandler} 
+                        type="button" 
+                        id="formButton" 
+                        name="button" 
+                        >
+                     Submit</button>
                 </div>
             </form>
         </section>
     );
 }
 
-export default contactForm;
+export default ContactForm;
