a994f6fca10abdbb517f57a99bdd83ab123e05b0
Domenico Capalbo <capalbodomenico@gmail.com>
2022-11-29 19:08:17 +0100

    fetching image string data from the db bug solved

--- fabrique-react/src/components/UI/multipleFilms/multipleFilms.js
+++ fabrique-react/src/components/UI/multipleFilms/multipleFilms.js
@@ -1,22 +1,27 @@
-import classes from './multipleFilms.module.scss';
+import classes from "./multipleFilms.module.scss";
 
 const multipleFilms = (props) => {
-    return(
-        <div className={classes.film__card__container}>
-            <img className={classes.film__card} src={props.imageUrl} alt={props.title} title={props.title} loading='lazy'/>
-            <div className={classes.film__card__description}>
-                <h2>{props.title}</h2>
-                <h4>{props.director}</h4>
-                <p>{props.description}</p>
-                <small>{props.duration}</small>
-                {/* <small>{props.lenght}</small> */}
-                {/* <small>{props.type}</small> */}
-                <div className={classes.film__card__button__wrapper}>
-                    <button className={classes.film__card__cta}>Scopri di più</button>
-                </div>
-            </div>
+  return (
+    <div className={classes.film__card__container}>
+      <img
+        className={classes.film__card}
+        src={props.imageUrl}
+        alt={props.title}
+        title={props.title}
+        loading="lazy"
+      />
+      <div className={classes.film__card__description}>
+        <h2>{props.title}</h2>
+        <h4>{props.director}</h4>
+        <p>{props.description}</p>
+        <small>{props.duration}</small>
+        {/* <small>{props.type}</small> */}
+        <div className={classes.film__card__button__wrapper}>
+          <button className={classes.film__card__cta}>Scopri di più</button>
         </div>
-    );
-}
+      </div>
+    </div>
+  );
+};
 
 export default multipleFilms;
--- fabrique-react/src/components/UI/multipleFilms/multipleFilmsDbContainer.js
+++ fabrique-react/src/components/UI/multipleFilms/multipleFilmsDbContainer.js
@@ -1,45 +1,47 @@
-import classes from './multipleFilmsContainer.module.scss';
-import PuffLoader from 'react-spinners/PuffLoader';
-import ApiHook from '../../../hooks/api-hook';
-import MultipleFilms from './multipleFilms';
+import classes from "./multipleFilmsContainer.module.scss";
+import base64ArrayBuffer from "../../../utils/base64";
+import PuffLoader from "react-spinners/PuffLoader";
+import ApiHook from "../../../hooks/api-hook";
+import MultipleFilms from "./multipleFilms";
 
 const MultipleFilmsDBContainer = () => {
+  const { fabriqueData, loading, error } = ApiHook(
+    "http://localhost:5000/get-films",
+    null
+  );
 
-    const { fabriqueData, loading, error } = ApiHook(
-        "http://localhost:5000/get-films", 
-        null
+  if (loading) {
+    return (
+      <PuffLoader
+        style={{
+          display: "inherit",
+          position: "relative",
+          width: "100px",
+          height: "100px",
+          margin: "auto",
+        }}
+        color={"#cc0000"}
+        size={100}
+      />
     );
-
-    if ( loading ) {
-        return (
-            <PuffLoader style={{ 
-                    display: 'inherit',
-                    position: 'relative',
-                    width: '100px',
-                    height: '100px',
-                    margin: 'auto'
-                }} color={'#cc0000'} size={100} 
-            />
-        )
-    } else if ( error ) {
-        <h1>There is some problem, please try to refresh</h1>
-    } else {
-        return (
-                <section className={classes.wrapper__films__container}>
-                    {fabriqueData.map(film => (
-                            <MultipleFilms
-                                description={film.description}
-                                director={film.director}
-                                imageUrl={film.imageUrl}
-                                duration={film.duration}
-                                title={film.title}
-                                key={film._id}
-                            /> 
-                        ))
-                    }
-                </section>
-        )
-    }
-}
+  } else if (error) {
+    <h1>There are some problem, please try to refresh</h1>;
+  } else {
+    return (
+      <section className={classes.wrapper__films__container}>
+        {fabriqueData.map((film) => (
+          <MultipleFilms
+            description={film.description}
+            director={film.director}
+            imageUrl={`data:image/png;base64,${base64ArrayBuffer(film)}`}
+            duration={film.duration}
+            title={film.title}
+            key={film._id}
+          />
+        ))}
+      </section>
+    );
+  }
+};
 
 export default MultipleFilmsDBContainer;
