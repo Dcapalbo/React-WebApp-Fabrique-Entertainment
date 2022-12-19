1e5ced2e3bb989257c63dffbe9f6adab1a010ac7
Domenico Capalbo <capalbodomenico@gmail.com>
2022-12-19 21:03:46 +0100

    fix

--- fabrique-react/src/utils/functions.js
+++ fabrique-react/src/utils/functions.js
@@ -1,22 +1,10 @@
-const descriptionLength = (value) =>
-  value.trim().length >= 10 && value.trim().length <= 300;
 const genericLength = (value) =>
   value.trim().length >= 3 && value.trim().length <= 30;
-const durationLength = (value) =>
-  value.trim().length >= 1 && value.trim().length <= 3;
-const yearLength = (value) => value.trim().length === 4;
 const isEmpty = (value) => value.trim() === "";
-const surnameLength = (value) =>
-  value.trim().length >= 3 && value.trim().length <= 20;
-const roleLength = (value) =>
-  value.trim().length >= 5 && value.trim().length <= 30;
-const bioLength = (value) =>
-  value.trim().length >= 10 && value.trim().length <= 300;
 const emailCheck = (value) =>
   value.trim().length > 10 &&
   value.trim().length < 40 &&
   value.trim().includes("@");
-const phoneNumberlength = (value) => value.length === 10;
 
 const decodeToken = (token) => {
   let base64Url = token.split(".")[1];
@@ -45,17 +33,4 @@ const cleanLocalStorage = () => {
   }
 };
 
-export {
-  descriptionLength,
-  phoneNumberlength,
-  cleanLocalStorage,
-  surnameLength,
-  genericLength,
-  durationLength,
-  roleLength,
-  decodeToken,
-  bioLength,
-  emailCheck,
-  yearLength,
-  isEmpty,
-};
+export { cleanLocalStorage, genericLength, decodeToken, emailCheck, isEmpty };
