6330ded7b059eb504b04be2c37aaee922bd3a683
Domenico Capalbo <capalbodomenico@gmail.com>
2023-04-20 19:26:13 +0300

    filmCardContainer removed from about page

--- fabrique-project/client/src/pages/About.jsx
+++ fabrique-project/client/src/pages/About.jsx
@@ -1,10 +1,9 @@
 import AboutCardContainer from "../components/UI/aboutCard/aboutCardContainer";
-import FilmCardContainer from "../components/UI/card/filmCardContainer";
 import CompanyInfo from "../components/UI/companyInfo/companyInfo";
 import { cleanLocalStorage } from "../utils/functions";
+import Footer from "../components/footer/footer";
 import Header from "../components/header/header";
 import Navigation from "../components/nav/nav";
-import Footer from "../components/footer/footer";
 
 const About = () => {
   cleanLocalStorage();
@@ -16,7 +15,6 @@ const About = () => {
       </Header>
       <AboutCardContainer />
       <CompanyInfo />
-      <FilmCardContainer />
       <Footer />
     </>
   );
