cb8848b59ee3fc23a51f9bb452b6b627a019b8cb
Domenico Capalbo <capalbodomenico@gmail.com>
2023-04-20 19:19:39 +0300

    token expiration fixed FE and BE

--- fabrique-project/client/src/utils/isAuth.js
+++ fabrique-project/client/src/utils/isAuth.js
@@ -8,9 +8,6 @@ const isAuth = (auth) => {
     if (Date.now() <= user.exp * 1000) {
       return true;
     } else if (Date.now() >= user.exp * 1000) {
-      console.log("FUNZIOOOOOONAAAAA IL TOKEN EXPIRAAAAATIOOON");
-      window.sessionStorage.removeItem(auth);
-      window.sessionStorage.removeItem("userId");
       window.location.replace("/login");
       return false;
     }
