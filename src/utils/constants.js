441a8d324165b77ae2ad5343317dc0c44396ab28
DomenicoReply <E5871121>
2025-11-13 02:47:20 +0200

    adding bobo into selects

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,42 +1,52 @@
 /** @format */
 
 // Accedi alle variabili d'ambiente usando import.meta.env
-const clientUrl = import.meta.env.VITE_NODE_ENV === 'production' ? import.meta.env.VITE_CLIENT_PRODUCTION_URL : import.meta.env.VITE_CLIENT_LOCAL_PORT;
+const clientUrl =
+  import.meta.env.VITE_NODE_ENV === 'production'
+    ? import.meta.env.VITE_CLIENT_PRODUCTION_URL
+    : import.meta.env.VITE_CLIENT_LOCAL_PORT
 
-const serverUrl = import.meta.env.VITE_NODE_ENV === 'production' ? import.meta.env.VITE_API_PRODUCTION_URL : import.meta.env.VITE_API_LOCAL_PORT;
+const serverUrl =
+  import.meta.env.VITE_NODE_ENV === 'production'
+    ? import.meta.env.VITE_API_PRODUCTION_URL
+    : import.meta.env.VITE_API_LOCAL_PORT
 
 const optionsFilms = [
-	{
-		value: '',
-		label: 'Tutti',
-	},
-	{
-		value: 'Lungometraggio',
-		label: 'Lungometraggio',
-	},
-	{
-		value: 'Cortometraggio',
-		label: 'Cortometraggio',
-	},
-	{
-		value: 'Documentario',
-		label: 'Documentario',
-	},
-];
+  {
+    value: '',
+    label: 'Tutti',
+  },
+  {
+    value: 'Lungometraggio',
+    label: 'Lungometraggio',
+  },
+  {
+    value: 'Cortometraggio',
+    label: 'Cortometraggio',
+  },
+  {
+    value: 'Documentario',
+    label: 'Documentario',
+  },
+]
 
 const optionsArticles = [
-	{
-		value: '',
-		label: 'Tutti',
-	},
-	{
-		value: 'Fela, il mio dio vivente',
-		label: 'Fela, il mio dio vivente',
-	},
-	{
-		value: 'Lucania',
-		label: 'Lucania',
-	},
-];
+  {
+    value: '',
+    label: 'Tutti',
+  },
+  {
+    value: 'Fela, il mio dio vivente',
+    label: 'Fela, il mio dio vivente',
+  },
+  {
+    value: 'Lucania',
+    label: 'Lucania',
+  },
+  {
+    value: 'Bobò',
+    label: 'Bobò',
+  },
+]
 
-export { clientUrl, serverUrl, optionsFilms, optionsArticles };
+export { clientUrl, serverUrl, optionsFilms, optionsArticles }
