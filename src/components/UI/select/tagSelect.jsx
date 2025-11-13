605f6b79b9740ab880f6352b6c3987d2a86c5ce8
DomenicoReply <E5871121>
2025-11-13 02:41:21 +0200

    integrazione nuova opzione bobo per select

--- src/components/UI/select/tagSelect.jsx
+++ src/components/UI/select/tagSelect.jsx
@@ -1,27 +1,24 @@
 /** @format */
 
-import React from 'react';
+import React from 'react'
 
 const TagSelect = ({ onChange, value }) => {
-	const options = [
-		{ value: '', label: 'Seleziona un opzione' },
-		{ value: 'Fela, il mio dio vivente', label: 'Fela, il mio dio vivente' },
-		{ value: 'Lucania', label: 'Lucania' },
-	];
+  const options = [
+    { value: '', label: 'Seleziona un opzione' },
+    { value: 'Fela, il mio dio vivente', label: 'Fela, il mio dio vivente' },
+    { value: 'Lucania', label: 'Lucania' },
+    { value: 'Bobò', label: 'Bobò' },
+  ]
 
-	return (
-		<select
-			onChange={onChange}
-			value={value}>
-			{options.map((option, index) => (
-				<option
-					key={index}
-					value={option.value}>
-					{option.label}
-				</option>
-			))}
-		</select>
-	);
-};
+  return (
+    <select onChange={onChange} value={value}>
+      {options.map((option, index) => (
+        <option key={index} value={option.value}>
+          {option.label}
+        </option>
+      ))}
+    </select>
+  )
+}
 
-export default TagSelect;
+export default TagSelect
